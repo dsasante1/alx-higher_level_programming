a4b98397e050da6cc821d9d9005f377113121e5a
K Asante <dsasante1@gmail.com>
2023-01-06 15:26:32 +0000

    fix file executable

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    arc = len(argv)
-    if arc > 2:
-        print("{:d} {:s}:".format(arc-1, "arguments"))
-        for i, s in enumerate(argv[1:]):
-            print("{:d}: {:s}".format(i+1, s))
-    if arc == 2:
-        print("1 argument:")
-        print("1: {}".format(argv[1]))
-    if arc == 1:
-        print("0 arguments.")
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{:d} arguments.".format(num - 1))
+    elif num == 2:
+        print("{:d} argument:".format(num - 1))
+    else:
+        print("{:d} arguments:".format(num - 1))
+
+    for i in range(1, num):
+        print("{:d}: {}".format(i, sys.argv[i]))
