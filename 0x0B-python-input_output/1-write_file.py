39d894d3525f1c00c1f14a524fe6659a1f6ac40d
K Asante <114578080+dsasante1@users.noreply.github.com>
2023-02-07 22:12:28 +0000

    fix 1-write_file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+number of lines
+"""
+
+
+def number_of_lines(filename=""):
+    """number of lines of a text file"""
+    with open(filename, "w", encoding="utf8") as file:
+        return file.write(text)
