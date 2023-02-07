1d1f9097b9581535a8e6dba1157b1416ad53bd47
K Asante <114578080+dsasante1@users.noreply.github.com>
2023-02-07 22:37:40 +0000

    json decode error 7-add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,6 @@ save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 try:
     lst = load_from_json_file("add_item.json")
-except ValueError:
+except JSONDecodeError:
     lst = []
 save_to_json_file(lst + sys.argv[1:], "add_item.json")
