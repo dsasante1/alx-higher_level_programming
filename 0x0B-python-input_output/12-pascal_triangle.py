8c377f4f168e3f84f4225101ae0f591d87929581
K Asante <114578080+dsasante1@users.noreply.github.com>
2023-02-08 00:42:11 +0000

    undo changes

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,15 +7,15 @@ def pascal_triangle(n):
 
     Returns a list of lists of integers representing the triangle.
     """
-      li = []
     if n <= 0:
-        return li
-    for r in range(n):
-        for c in range(r + 1):
-            if c == 0:
-                li.append([1])
-            elif c == r:
-                li[r].append(1)
-            else:
-                li[r].append(li[r - 1][c] + li[r - 1][c - 1])
-    return li
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
